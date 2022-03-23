0b52080714519c01748759669e7c2efc1352b8f3
leman ibrahimli <leman.ibrahimli23@gmail.com>
2022-03-23 19:10:28 +0400

    l

--- pages/[productId].js
+++ pages/[productId].js
@@ -1,14 +1,11 @@
 import React from "react";
-import NavbarForAllPage from "../components/Layout/components/AllHeader/NavbarForAllPage";
-import MobileMenu from "../components/Layout/components/AllHeader/MobileMenu";
-import MainForProduct from "../components/Layout/components/MainForProduct/MainForProduct";
 
 export default function productId() {
     return (
         <div>
-            <NavbarForAllPage/>
-            <MobileMenu/>
-            <MainForProduct/>
+            {/*<NavbarForAllPage/>*/}
+            {/*<MobileMenu/>*/}
+            {/*<MainForProduct/>*/}
         </div>
     )
 }
